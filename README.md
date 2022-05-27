fcc869044b7232fa26ea825116c0aeb455afdf43
Harmeet Matharoo <hmjatt@gmail.com>
2022-05-27 17:51:04 -0600

    updated readme and email validation

--- README.md
+++ README.md
@@ -11,8 +11,39 @@ A responsive sign-up form. Feel free to give me reviews at my [Twitter](https://
 
 <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank" rel="noreferrer"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-original.svg" alt="javascript" width="30" height="30"/> </a>  &emsp;   <a href="https://www.w3.org/html/" target="_blank" rel="noreferrer"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/html5/html5-original-wordmark.svg" alt="html5" width="30" height="30"/> </a>  &emsp;   <a href="https://www.w3schools.com/css/" target="_blank" rel="noreferrer"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/css3/css3-original-wordmark.svg" alt="css3" width="30" height="30"/> </a>
 
+### Updates made to old project
 
-    A computer science teacher asks the class to turn to page 404.
-    The students search feverishly, to no avail
+1. Form Validation with JavaScript
+
+2. Custom error messages
+
+3. Password and Confirm Password validation with custom error message
+
+4. Email validation with custom error message
+
+5. Min & Max Length validation for Name, Email, Password, Confirm Password
+
+
+### Links to content that helped me with this project
+
+1. MDN
+    - [Client-side Form Validation](https://developer.mozilla.org/en-US/docs/Learn/Forms/Form_validation#validating_forms_using_javascript)
+
+3. The Odin Project
+    - [Article](https://www.theodinproject.com/lessons/node-path-javascript-form-validation-with-javascript)
+
+4. W3Schools
+    - [JavaScript Validation Api](https://www.w3schools.com/js/js_validation_api.asp)
+	- [Custom Password Validation](https://www.w3schools.com/howto/howto_js_password_validation.asp)
+
+5. Stack Overflow
+	- [List of standard lengths for database fields](https://stackoverflow.com/questions/20958/list-of-standard-lengths-for-database-fields)
+
+
+#### Quote
+
+    "A computer science teacher asks the class to turn to page 404.
+    The students search feverishly, to no avail"
+
 >  	
 > :smile:	:wavy_dash:
--- script.js
+++ script.js
@@ -1,1 +1,61 @@
 // Sign-up Form
+
+
+//for email validation
+
+// There are many ways to pick a DOM node; here we get the form itself and the email
+// input box, as well as the span element into which we will place the error message.
+const form = document.getElementsByTagName('form')[0];
+
+const email = document.getElementById('mail');
+const error = document.getElementById('error');
+const emailError = document.querySelector('#mail + span.error');
+
+email.addEventListener('input', function (event) {
+	// Each time the user types something, we check if the
+	// form fields are valid.
+
+	error.style.display = 'block';
+	// If the email field is empty, show the error message
+
+	if (email.validity.valid) {
+		// In case there is an error message visible, if the field
+		// is valid, we remove the error message.
+		emailError.innerHTML = ''; // Reset the content of the message
+		emailError.className = 'error'; // Reset the visual state of the message
+	} else {
+		// If there is still an error, show the correct error
+		showError();
+	}
+});
+
+form.addEventListener('submit', function (event) {
+	// if the form contains valid data, we let it submit
+
+	if (!email.validity.valid) {
+		// If it isn't, we display an appropriate error message
+		showError();
+		// Then we prevent the form from being sent by canceling the event
+		event.preventDefault();
+	}
+});
+
+function showError() {
+	if (email.validity.valueMissing) {
+		// If the field is empty
+		// display the following error message.
+		emailError.textContent = 'You need to enter an e-mail address.';
+	} else if (email.validity.typeMismatch) {
+		// If the field doesn't contain an email address
+		// display the following error message.
+		emailError.textContent = 'Entered value needs to be an e-mail address.';
+	} else if (email.validity.tooShort) {
+		// If the data is too short
+		// display the following error message.
+		emailError.textContent = `Email should be at least ${email.minLength} characters; you entered ${email.value.length}.`;
+	}
+
+	// Set the styling appropriately
+	emailError.className = 'error active';
+
+}
